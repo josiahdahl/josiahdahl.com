0e5c2ebbc4793a92b22bd7375f7e59b98356d548
Josiah Dahl <josiah.dahl@gmail.com>
2018-09-04 10:41:43 -0700

    feat(404): Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Section from '../components/ui/Section';
+
+const FourOhFour = () => {
+  return (
+    <div>
+      <Section>
+        <h1>Page not found :(</h1>
+      </Section>
+    </div>
+  );
+};
+
+export default FourOhFour;
